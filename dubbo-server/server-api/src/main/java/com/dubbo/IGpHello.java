8c739e3f4729ffbd500df689867a55ce04f01e01
sunyk <350052062@qq.com>
2018-07-15 14:37:47 +0800

    dubbotest

---
+++ dubbo-server/server-api/src/main/java/com/dubbo/IGpHello.java
@@ -0,0 +1,9 @@
+package com.dubbo;
+
+/**
+ * Create by sunyang on 2018/6/24 15:54
+ * For me:One handred lines of code every day,make myself stronger.
+ */
+public interface IGpHello {
+    String sayHello(String msg);
+}
---
+++ dubbo-server/server-provider/src/main/java/com/dubbo/GpHelloImpl2.java
@@ -0,0 +1,13 @@
+package com.dubbo;
+
+/**
+ * Create by sunyang on 2018/6/24 15:47
+ * For me:One handred lines of code every day,make myself stronger.
+ */
+public class GpHelloImpl2 implements IGpHello{
+    @Override
+    public String sayHello(String msg) {
+        System.out.println("Server receive" + msg);
+        return "Hello,version2.0" + msg;
+    }
+}
