32e6c7ab2efc8eb165b7fd0ee7cee97a724d2f64
sunyk <350052062@qq.com>
2018-08-05 15:13:22 +0800

    mybatis-generator for my

---
+++ mybatis-generator/src/main/java/com/sunyk/dal/dao/Test.java
@@ -0,0 +1,33 @@
+package com.sunyk.dal.dao;
+
+public class Test {
+    private Integer id;
+
+    private String name;
+
+    private Short age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+}
